7ef363b26e1c333593a13601ff3c495a1d49641e
Divyapriyadharshini Babu <divyapriyadharshinibabu@gmail.com>
2022-07-03 10:30:48 -0700

    Use DOM traversal and add/remove classes for each time period

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,20 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd[,] MMMM Do"));
+
+$('.time-block').each(function() {
+    var hourBlock = parseInt($(this).attr('id'));
+    var currentHour = today.hour();
+    if (hourBlock < currentHour) {
+        $(this).children().eq(1).addClass("past");
+        $(this).children().eq(1).removeClass("future");
+        $(this).children().eq(1).removeClass("present");
+    } else if (hourBlock == currentHour) {
+        $(this).children().eq(1).addClass("present");
+        $(this).children().eq(1).removeClass("past");
+        $(this).children().eq(1).removeClass("future");
+    } else {
+        $(this).children().eq(1).addClass("future");
+        $(this).children().eq(1).removeClass("past");
+        $(this).children().eq(1).removeClass("present");
+    }
+});
