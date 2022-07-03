71d8882c7bc850f2665cdce56351edb64ee2a4d8
Divyapriyadharshini Babu <divyapriyadharshinibabu@gmail.com>
2022-07-03 11:11:42 -0700

    Use localStorage to set and get the event for the corresponding time block

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,19 @@ $('.time-block').each(function() {
         $(this).children().eq(1).removeClass("present");
     }
 });
+
+$('.saveBtn').on('click', function () {
+    var parent = $(this).parent();
+
+    localStorage.setItem(parent.attr('id'), parent.children().eq(1).val());
+});
+
+
+$('#10').children().eq(1).val(localStorage.getItem(10));
+$('#11').children().eq(1).val(localStorage.getItem(11));
+$('#12').children().eq(1).val(localStorage.getItem(12));
+$('#13').children().eq(1).val(localStorage.getItem(13));
+$('#14').children().eq(1).val(localStorage.getItem(14));
+$('#15').children().eq(1).val(localStorage.getItem(15));
+$('#16').children().eq(1).val(localStorage.getItem(16));
+$('#17').children().eq(1).val(localStorage.getItem(17));
